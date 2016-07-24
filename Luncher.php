2cb4709b62ed4ef69a59b2a5ca62ac6ccabc1c75
TheDeSigN3r <s.h1459@yahoo.com>
2016-07-24 10:29:46 +0000

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '229159675:AAEbg4oZW-YxsHSS1_VcEDlUwWclNEFUDDY');
+define('BOT_TOKEN', '229159675:AAGwsRwKSc6ifd3xy46OlzQNsLNuhv-BwtQ');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
